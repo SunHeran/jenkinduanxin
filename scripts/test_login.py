ef306913077de738db48924085d86ddf78a904af
SunHeran <864760526@qq.com>
2019-05-06 20:45:06 +0800

    fist duanxins

---
+++ scripts/test_login.py
@@ -0,0 +1,22 @@
+import pytest
+
+
+class TestLogin:
+    def test_login1(self):
+        print("test_login1")
+        assert 1
+
+    @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
+    def test_login2(self):
+        print("test_login2")
+        assert 1
+
+    @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
+    def test_login3(self):
+        print("test_login3")
+        assert 0
+
+    @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
+    def test_login4(self):
+        print("test_login4")
+        assert 1
